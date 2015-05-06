f0720e8cbd04247dffb4aacf03113a16890e5ef9
ZHANG Ta <coolgene@gmail.com>
2015-05-06 23:07:38 +0800

    把tab改成空格（因为所有其他文件都是空格啊！）
    所有其他的文件的缩进，比如
    settings.py、urls.py等等，全都是用空格缩进的，咱们为了保持一致，也用空格吧……关于这个问题的讨论，请见：http://segmentfault.com/q/1010000000158915

--- Waihui/main/models.py
+++ Waihui/main/models.py
@@ -4,93 +4,93 @@ from django.db import models
 # Create your models here.
 class Buyer(models.Model):
 
-	class Meta:
-		verbose_name = "Buyer"
-		verbose_name_plural = "Buyers"
+    class Meta:
+        verbose_name = "Buyer"
+        verbose_name_plural = "Buyers"
 
-	def __str__(self):
-		pass
-	user = models.OneToOneField(User)
-	nickname = models.CharField(required=True, max_length=50)
-	brithday = models.DateField()
-	mother_tongue = models.ForeignKey(Language)
-	provider = models.ForeignKey(Provider)
-	time_zone = models.CharField(, max_length=50)
+    def __str__(self):
+        pass
+    user = models.OneToOneField(User)
+    nickname = models.CharField(required=True, max_length=50)
+    brithday = models.DateField()
+    mother_tongue = models.ForeignKey(Language)
+    provider = models.ForeignKey(Provider)
+    time_zone = models.CharField(, max_length=50)
 
 class Provider(models.Model):
 
-	class Meta:
-		verbose_name = "Provider"
-		verbose_name_plural = "Providers"
+    class Meta:
+        verbose_name = "Provider"
+        verbose_name_plural = "Providers"
 
-	def __str__(self):
-		pass
-	user = models.OneToOneField(User)
-	ROOKIE = 1
-	APPLIED = 2
-	INTERN = 3
-	FORMAL = 4
-	LEVELS_OF_TEACHER_CHOICES = (
-		(ROOKIE,'非教师'),
-		(APPLIED,'已申请'),
-		(INTERN,'实习'),
-		(FORMAL,'正式'),
-	)
-	status = models.IntegerField(max_length=2,
-		choices='LEVELS_OF_TEACHER_CHOICES', 
-		default=ROOKIE)
-	name = models.CharField(required=True, max_length=50)
-	weekday_pattern = models.CommaSeparatedIntegerField()
+    def __str__(self):
+        pass
+    user = models.OneToOneField(User)
+    ROOKIE = 1
+    APPLIED = 2
+    INTERN = 3
+    FORMAL = 4
+    LEVELS_OF_TEACHER_CHOICES = (
+        (ROOKIE,'非教师'),
+        (APPLIED,'已申请'),
+        (INTERN,'实习'),
+        (FORMAL,'正式'),
+    )
+    status = models.IntegerField(max_length=2,
+        choices='LEVELS_OF_TEACHER_CHOICES', 
+        default=ROOKIE)
+    name = models.CharField(required=True, max_length=50)
+    weekday_pattern = models.CommaSeparatedIntegerField()
 
 class Sku(models.Model):
 
-	class Meta:
-		verbose_name = "Sku"
-		verbose_name_plural = "Skus"
+    class Meta:
+        verbose_name = "Sku"
+        verbose_name_plural = "Skus"
 
-	def __str__(self):
-		pass
-	provider = models.ForeignKey(Provider, required=True)
-	buyer = models.ForeignKey(Buyer)
-	FORBOOK = 1
-	PREBOOKED = 2
-	BOOKED = 3
-	FINISHED = 4
-	STATUES_OF_SKU_CHOICES = (
-		(FORBOOK,'可预约'),
-		(PREBOOKED,'已预约'),
-		(BOOKED,'已定'),
-		(FINISHED,'已结束'),
-	)
-	statue = models.IntegerField(required=True,
-		max_length=2,
-		choices='STATUES_OF_SKU_CHOICES',
-	)
-		start_time = models.DateTimeField()
-		end_time = models.DateTimeField()
-		topic = models.ForeignKey(Topic)
+    def __str__(self):
+        pass
+    provider = models.ForeignKey(Provider, required=True)
+    buyer = models.ForeignKey(Buyer)
+    FORBOOK = 1
+    PREBOOKED = 2
+    BOOKED = 3
+    FINISHED = 4
+    STATUES_OF_SKU_CHOICES = (
+        (FORBOOK,'可预约'),
+        (PREBOOKED,'已预约'),
+        (BOOKED,'已定'),
+        (FINISHED,'已结束'),
+    )
+    statue = models.IntegerField(required=True,
+        max_length=2,
+        choices='STATUES_OF_SKU_CHOICES',
+    )
+        start_time = models.DateTimeField()
+        end_time = models.DateTimeField()
+        topic = models.ForeignKey(Topic)
 
 class Language(models.Model):
 
-	class Meta:
-		verbose_name = "Language"
-		verbose_name_plural = "Languages"
+    class Meta:
+        verbose_name = "Language"
+        verbose_name_plural = "Languages"
 
-	def __str__(self):
-		pass
-	chinese_name = models.CharField(required=True, max_length=50)
-	english_name = models.CharField(required=True, max_length=50)
-	local_name = models.CharField(required=True, max_length=50)
+    def __str__(self):
+        pass
+    chinese_name = models.CharField(required=True, max_length=50)
+    english_name = models.CharField(required=True, max_length=50)
+    local_name = models.CharField(required=True, max_length=50)
 
 class Topic(models.Model):
-	
-	    class Meta:
-	        verbose_name = "Topic"
-	        verbose_name_plural = "Topics"
-	
-	    def __str__(self):
-	        pass
+    
+        class Meta:
+            verbose_name = "Topic"
+            verbose_name_plural = "Topics"
+    
+        def __str__(self):
+            pass
     name = models.CharField(required=True, null=False, max_length=50)
-	category = models.CharField(required=True, max_length=50)
+    category = models.CharField(required=True, max_length=50)
 
-	
+    
