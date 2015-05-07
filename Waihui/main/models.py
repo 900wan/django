f1d6fda0a510b23f7fe74d409ed13ebe7b286cc9
cjwn <atom.cn@gmail.com>
2015-05-07 23:53:04 +0800

    Modify def __str__ into unicode
    只是简单的返回了某一个参数，出SKU以外并没有复合显示

--- Waihui/main/models.py
+++ Waihui/main/models.py
@@ -8,8 +8,8 @@ class Buyer(models.Model):
         verbose_name = "Buyer"
         verbose_name_plural = "Buyers"
 
-    def __str__(self):
-        pass
+    def __unicode__(self):
+        return self.nickname
     user = models.OneToOneField(User)
     nickname = models.CharField(required=True, max_length=50)
     brithday = models.DateField()
@@ -17,14 +17,15 @@ class Buyer(models.Model):
     provider = models.ForeignKey(Provider)
     time_zone = models.CharField(, max_length=50)
 
+
 class Provider(models.Model):
 
     class Meta:
         verbose_name = "Provider"
         verbose_name_plural = "Providers"
 
-    def __str__(self):
-        pass
+    def __unicode__(self):
+        return self.name
     user = models.OneToOneField(User)
     ROOKIE = 1
     APPLIED = 2
@@ -42,16 +43,18 @@ class Provider(models.Model):
     name = models.CharField(required=True, max_length=50)
     weekday_pattern = models.CommaSeparatedIntegerField()
 
+
 class Sku(models.Model):
 
     class Meta:
         verbose_name = "Sku"
         verbose_name_plural = "Skus"
 
-    def __str__(self):
-        pass
+    def __unicode__(self):
+        return "("+u(self.start_time)+")"+self.topic
     provider = models.ForeignKey(Provider, required=True)
     buyer = models.ForeignKey(Buyer)
+    
     FORBOOK = 1
     PREBOOKED = 2
     BOOKED = 3
@@ -62,13 +65,16 @@ class Sku(models.Model):
         (BOOKED,'已定'),
         (FINISHED,'已结束'),
     )
-    statue = models.IntegerField(required=True,
+    
+    statue = models.IntegerField(
+        required=True,
         max_length=2,
         choices='STATUES_OF_SKU_CHOICES',
     )
-        start_time = models.DateTimeField()
-        end_time = models.DateTimeField()
-        topic = models.ForeignKey(Topic)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    topic = models.ForeignKey(Topic)
+
 
 class Language(models.Model):
 
@@ -76,22 +82,24 @@ class Language(models.Model):
         verbose_name = "Language"
         verbose_name_plural = "Languages"
 
-    def __str__(self):
-        pass
+    def __unicode__(self):
+        return self.english_name
     chinese_name = models.CharField(required=True, max_length=50)
     english_name = models.CharField(required=True, max_length=50)
     local_name = models.CharField(required=True, max_length=50)
 
+
 class Topic(models.Model):
     
-        class Meta:
-            verbose_name = "Topic"
-            verbose_name_plural = "Topics"
+    class Meta:
+        verbose_name = "Topic"
+        verbose_name_plural = "Topics"
     
-        def __str__(self):
-            pass
+    def __unicode__(self):
+        return self.name
     name = models.CharField(required=True, null=False, max_length=50)
     category = models.ForeignKey(TopicCategory)
+
 
 class TopicCategory(models.Model):
 
@@ -99,10 +107,11 @@ class TopicCategory(models.Model):
         verbose_name = "TopicCategory"
         verbose_name_plural = "TopicCategorys"
 
-    def __str__(self):
-        pass
+    def __unicode__(self):
+        return self.name
     name = models.CharField(required=True, max_length=50)
     background_image = models.URLField()
+
 
 class Wallet(models.Model):
 
@@ -110,20 +119,21 @@ class Wallet(models.Model):
         verbose_name = "Wallet"
         verbose_name_plural = "Wallets"
 
-    def __str__(self):
-        pass
+    def __unicode__(self):
+        return self.cny_balance
     user = models.ForeignKey(User)
     cny_balance = models.FloatField(required=True, default=0)
     display_currency = models.CharField(required=True, default= "cny" , max_length=50)
     
+
 class ReviewTovProvider(models.Model):
 
     class Meta:
         verbose_name = "ReviewTovProvider"
         verbose_name_plural = "ReviewTovProviders"
 
-    def __str__(self):
-        pass
+    def __unicode__(self):
+        return self.score
     from = models.ForeignKey(User)
     to = models.ForeignKey(User)
     sku = models.ForeignKey(Sku)
