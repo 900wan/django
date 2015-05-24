42b8f4734611d77d36b5541fa6af7f944692be67
cjwn <atom.cn@gmail.com>
2015-05-24 19:20:51 +0800

    First 自检

--- Waihui/main/models.py
+++ Waihui/main/models.py
@@ -1,24 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.db import models
-from django.contrib.auth.models import all
+from django.contrib.auth.models import *
 
 # Create your models here.
-class Buyer(models.Model):
-
-    class Meta:
-        verbose_name = "Buyer"
-        verbose_name_plural = "Buyers"
-
-    def __unicode__(self):
-        return self.nickname
-    user = models.OneToOneField(User)
-    nickname = models.CharField(required=True, max_length=50)
-    brithday = models.DateField()
-    mother_tongue = models.ForeignKey(Language)
-    provider = models.ForeignKey(Provider)
-    time_zone = models.CharField(, max_length=50)
-
-
 class Provider(models.Model):
 
     class Meta:
@@ -39,10 +23,70 @@ class Provider(models.Model):
         (FORMAL,'正式'),
     )
     status = models.IntegerField(max_length=2,
-        choices='LEVELS_OF_TEACHER_CHOICES', 
+        choices=LEVELS_OF_TEACHER_CHOICES, 
         default=ROOKIE)
-    name = models.CharField(required=True, max_length=50)
-    weekday_pattern = models.CommaSeparatedIntegerField()
+    name = models.CharField(max_length=50, )
+    weekday_pattern = models.CommaSeparatedIntegerField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+
+class Language(models.Model):
+
+    class Meta:
+        verbose_name = "Language"
+        verbose_name_plural = "Languages"
+
+    def __unicode__(self):
+        return self.english_name
+    chinese_name = models.CharField( max_length=50)
+    english_name = models.CharField( max_length=50)
+    local_name = models.CharField( max_length=50)
+
+
+class Buyer(models.Model):
+
+    class Meta:
+        verbose_name = "Buyer"
+        verbose_name_plural = "Buyers"
+
+    def __unicode__(self):
+        return self.nickname
+    user = models.OneToOneField(User)
+    nickname = models.CharField(max_length=50)
+    brithday = models.DateField()
+    mother_tongue = models.ForeignKey(Language)
+    time_zone = models.CharField(max_length=50)
+    hp = models.IntegerField()
+    create_time = models.DateTimeField()
+    modity_time = models.DateTimeField()
+
+
+class TopicCategory(models.Model):
+
+    class Meta:
+        verbose_name = "TopicCategory"
+        verbose_name_plural = "TopicCategorys"
+
+    def __unicode__(self):
+        return self.name
+    name = models.CharField( max_length=50)
+    background_image = models.URLField()
+
+
+class Topic(models.Model):
+
+    class Meta:
+        verbose_name = "Topic"
+        verbose_name_plural = "Topics"
+
+    def __str__(self):
+        pass
+    name = models.CharField(max_length=50)
+    category = models.ForeignKey(TopicCategory)
+    # default_plan = models.ForeignKey(Plan)
+    status = models.IntegerField()
+    creator = models.ForeignKey(User)
 
 
 class Sku(models.Model):
@@ -53,7 +97,7 @@ class Sku(models.Model):
 
     def __unicode__(self):
         return "("+u(self.start_time)+")"+self.topic
-    provider = models.ForeignKey(Provider, required=True)
+    provider = models.ForeignKey(Provider, )
     buyer = models.ForeignKey(Buyer)
     
     FORBOOK = 1
@@ -68,9 +112,9 @@ class Sku(models.Model):
     )
     
     status = models.IntegerField(
-        required=True,
         max_length=2,
-        choices='STATUS_OF_SKU_CHOICES',
+        choices=STATUS_OF_SKU_CHOICES,
+        default=FORBOOK,
     )
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
@@ -85,59 +129,18 @@ class Plan(models.Model):
 
     def __unicode__(self):
         pass
-    sku = models.OneToOneField()
-    topic = models.ForeignKey(Topic, required=True)
-    status = models.IntegerField(required=True)
-    content = models.TextField(required=True)
+    sku = models.OneToOneField(Sku)
+    topic = models.ForeignKey(Topic, )
+    status = models.IntegerField()
+    content = models.TextField()
     assignment = models.TextField()
     slides = models.TextField()
     materiallinks = models.TextField()
     materialhtml = models.TextField()
     voc = models.TextField()
-    copy_from = models.ForeignKey(Plan)
+    copy_from = models.ForeignKey('self')
     # summary 写sum我怕出问题
     sumy = models.TextField()
-
-
-class Language(models.Model):
-
-    class Meta:
-        verbose_name = "Language"
-        verbose_name_plural = "Languages"
-
-    def __unicode__(self):
-        return self.english_name
-    chinese_name = models.CharField(required=True, max_length=50)
-    english_name = models.CharField(required=True, max_length=50)
-    local_name = models.CharField(required=True, max_length=50)
-
-
-class Topic(models.Model):
-    
-    class Meta:
-        verbose_name = "Topic"
-        verbose_name_plural = "Topics"
-    
-    def __unicode__(self):
-        return self.name
-    name = models.CharField(required=True, null=False, max_length=50)
-    category = models.ForeignKey(TopicCategory)
-    default_plan = models.ForeignKey(Plan)
-    status = models.IntegerField(required=True)
-    creator = models.ForeignKey(User)
-
-
-
-class TopicCategory(models.Model):
-
-    class Meta:
-        verbose_name = "TopicCategory"
-        verbose_name_plural = "TopicCategorys"
-
-    def __unicode__(self):
-        return self.name
-    name = models.CharField(required=True, max_length=50)
-    background_image = models.URLField()
 
 
 class Wallet(models.Model):
@@ -149,8 +152,8 @@ class Wallet(models.Model):
     def __unicode__(self):
         return self.cny_balance
     user = models.ForeignKey(User)
-    cny_balance = models.FloatField(required=True, default=0)
-    display_currency = models.CharField(required=True, default= "CNY" , max_length=50)
+    cny_balance = models.FloatField(default=0)
+    display_currency = models.CharField( default= "CNY" , max_length=50)
     
 
 class ReviewTovProvider(models.Model):
@@ -164,9 +167,10 @@ class ReviewTovProvider(models.Model):
     provider = models.ForeignKey(Provider)
     buyer = models.ForeignKey(Buyer)
     sku = models.OneToOneField(Sku)
-    questionnaire = models.CharField(required=True, max_length=50)
+    questionnaire = models.CharField( max_length=50)
     comment = models.CharField(max_length=50)
-    score = models.FloatField(required=True)
+    score = models.FloatField()
+
 
 class ReviewToBuyer(models.Model):
 
@@ -179,8 +183,8 @@ class ReviewToBuyer(models.Model):
     provider = models.ForeignKey(Provider)
     buyer = models.ForeignKey(Buyer)
     sku = models.OneToOneField(Sku)
-    questionnaire = models.CharField(, max_length=50)
-    comment = models.CharField(, max_length=50)
+    questionnaire = models.CharField( max_length=50)
+    comment = models.CharField(max_length=50)
 
 
 class ReplyToSku(models.Model):
@@ -192,14 +196,11 @@ class ReplyToSku(models.Model):
     def __unicode__(self):
         pass
     from_user = models.ForeignKey(User)
-    from_type = models.IntegerField(required=True)
-    content = models.TextField(required=True)
-    to_reply = models.ForeignKey(Reply)
+    from_type = models.IntegerField()
+    content = models.TextField()
+    to_reply = models.ForeignKey('self')
 
 # TODO 有空时咱们一起进行：
-# 调整 modles 的顺序（被引用到的应该先现）
-# str 改 unicode
-# ForeignKey 有些没有填写
 # 默认值、是否必填等有些还需要再调整
 # max_length长度有些字段可能不够
 # 添加日期、修改日期回头统一给每一个 model 加
