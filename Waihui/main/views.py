286505980fa6d405bd7bbfe03c2898624cf7b77d
cjwn <atom.cn@gmail.com>
2015-06-17 19:10:35 +0800

    built some views for test

--- Waihui/main/views.py
+++ Waihui/main/views.py
@@ -1,3 +1,18 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 
 # Create your views here.
+def ds_checkrequest_form():
+    if request.method == 'POST':
+        return "good condition"
+
+def act_signup():
+    ds_checkrequest_form
+
+def url_signup_post(request):
+# "用户通过浏览器将表单内容post到/signup/post后来到这里"
+    act_signup
+    ds_
+
+def url_index():
+    return "This is index."
