d778521e9efc0b5254a2bfb39071c40c6d51087e
cjwn <atom.cn@gmail.com>
2015-06-18 21:44:50 +0800

    build some view for test

--- Waihui/main/views.py
+++ Waihui/main/views.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def ds_checkrequest_form():
     if request.method == 'POST':
-        return "good condition"
+        return HttpResponse("good condition")
+    else :
+        ds_return_check
+    return ds_return_check    
+
+def ds_return_check():
+    return HttpResponse('this is used by return_check')
 
 def act_signup():
     ds_checkrequest_form
@@ -12,7 +19,8 @@ def act_signup():
 def url_signup_post(request):
 # "用户通过浏览器将表单内容post到/signup/post后来到这里"
     act_signup
-    ds_
+    
 
-def url_index():
-    return "This is index."
+def url_index(request):
+    # return render(request, 'pass', )
+    return HttpResponse('Question')
