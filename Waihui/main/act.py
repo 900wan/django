873876e49d11a28edcfe8b61150895e9b4befa15
cjwn <atom.cn@gmail.com>
2015-06-21 01:00:10 +0800

    Add some act_add

--- Waihui/main/act.py
+++ Waihui/main/act.py
@@ -2,6 +2,10 @@
 from main.models import User
 from main.models import Buyer
 from main.models import Provider
+from main.models import Topic
+from main.models import TopicCategory
+from main.models import Sku
+from main.models import Plan
 from main.models import Wallet
 from main.models import Language
 
@@ -31,7 +35,7 @@ def act_signup(email,password,nickname,gender,mother_tongue_id,time_zone):
         user = user)
     wallet.save()
 
-    result="OK!" + str(user.username) + "." + str(buyer.nickname) + "has added"
+    result="OK!" + str(username) + "." + str(nickname) + "has added"
     # except:
     #     pass
     return result
@@ -42,8 +46,57 @@ def act_addlanguage(chinese_name, english_name, local_name):
         english_name = english_name,
         local_name = local_name,)
     language.save()
-    result = "OK, " + local_name + " added!"
+    result = "OK, Language:" + local_name + " added!"
     return result
+
+def act_addtopiccategory(name, url):
+    topiccategory = TopicCategory(
+        name = name,
+        url = url)
+    topiccategory.save()
+    result = "OK, Topic category:" + name + " added!"
+    return result
+
+def act_addtopic(name, category_id, status, user_id):
+    topic = Topic(
+        name = name,
+        category = category_id,
+        status = status,
+        creator = user_id)
+    topic.save()
+    result = "OK, Topic:" + name + " added!"
+    return result
+
+def act_addsku(provider_id, status, start_time, end_time, topic_id,):
+    sku = Sku(
+        provider = provider_id,
+        status = status,
+        start_time = start_time,
+        end_time = end_time,
+        topic = topic_id)
+    sku.save()
+    provider = Provider.objects.get(id = provider_id)
+    topic = Topic.objects.get(id = topic_id)
+    result = "OK, Sku:" + provider.name +"'s "+ topic.name + str(start_time) + " added!"
+    return result
+
+def act_addplan(sku_id, topic_id, status, content,):
+    plan = Plan.objects(
+        sku = sku_id,
+        topic = topic_id,
+        status = status,
+        content = content)
+    plan.save()
+    sku = Sku.objects.get(id = sku_id)
+    topic = Topic.objects.get(id = topic_id)
+    result = "OK, Plan: " + sku.provider + topic.name + " added!"
+    return result
+
+def function():
+    pass
+
+def act_updatewallet():
+    pass
 
 def act_showuser(id):
     user = User.objects.get(id = id),
