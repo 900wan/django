b9c848665ab8bbbe3b134efd6202e3c9b743716b
cjwn <atom.cn@gmail.com>
2015-07-19 17:58:36 +0800

    a sync

--- Waihui/main/ds.py
+++ Waihui/main/ds.py
@@ -7,7 +7,13 @@
 #         return "else return"
 #     return 
 
+from main.models import Topic
     
-def ds_jisuan(bad):
-    arc=bad+28
-    return arc
+def ds_showtopic(id=0, bywhat=0):
+    if bywhat == 0:
+        topic = Topic.objects.get(id=id)
+    elif id == 0:
+        topic = Topic.objects.order_by('bywhat')
+    elif bywhat == 0 & id == 0:
+        topic = "error"
+    return topic
