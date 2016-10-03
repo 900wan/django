d46695f5f7bc1a183706469f6ae9996a6a0c1354
cjwn <atom.cn@gmail.com>
2016-10-03 17:15:17 +0800

    updates

--- Waihui/minutes/urls.py
+++ Waihui/minutes/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<entry_id>[0-9]+)/$', views.entry_detail, name='entry_detail'),
     url(r'^(?P<entry_id>[0-9]+)/signin$', views.easy_signin, name='easy_signin'),
-    url(r'^(?p<entry_id>[0-9]+)/wxsignin$', views.wechat_signin, name='wechat_signin'),
+    url(r'^(?P<entry_id>[0-9]+)/wxsignin$', views.wechat_signin, name='wechat_signin'),
 ]
