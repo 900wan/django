a3f235be2bf3db24ca39650ef4db04ca5f15b953
cjwn <atom.cn@gmail.com>
2016-10-03 17:11:27 +0800

    WeChat signin step 1

--- Waihui/minutes/views.py
+++ Waihui/minutes/views.py
@@ -48,3 +48,8 @@ def easy_signin(request, entry_id):
         uf = AttendForm()
         result = "请将参会信息填写完整"
     return render(request, "easy_signin.html", locals())
+
+def wechat_signin(request):
+    if request.method == 'GET':
+        code = request.GET['CODE']
+    return HttpResponseRedirect("https://api.weixin.qq.com/sns/oauth2/access_token?appid=wxd78d86d0ebcc727b&secret=222b3622f6f6070eb408a244d9d142a7&code="&code&"&grant_type=authorization_code ")
