c0ac42bf9b52b20d9489658eb00420277f1a6589
cjwn <atom.cn@gmail.com>
2016-10-18 15:06:48 +0800

    针对profile的所参与会的名称进行修改
    entry->attended_entries

--- Waihui/minutes/models.py
+++ Waihui/minutes/models.py
@@ -51,6 +51,6 @@ class Profile(models.Model):
     phonenumber = models.CharField(u'手机', max_length=50, blank=True, null=True)
     wx_id = models.CharField(u'微信', max_length=100, blank=True, null=True)
     user = models.OneToOneField(User, blank=True, null=True)
-    entry = models.ManyToManyField(Entry)
+    attended_entries = models.ManyToManyField(Entry, verbose_name ="出席的会议", related_name="attendee")
     def __unicode__(self):
         return u'%s' % self.display_name
