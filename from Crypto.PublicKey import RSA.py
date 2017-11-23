de11a8292b152ba53b74091ace4973e720bd6c9a
cjwn <atom.cn@gmail.com>
2017-11-23 20:06:43 +0800

    little sync

--- from Crypto.PublicKey import RSA.py
+++ from Crypto.PublicKey import RSA.py
@@ -28,13 +28,26 @@ def sign_read_type2(data):
     signature = signer.sign(h)
     return b64encode(signature)
 
+def sign_string(private_key_path, unsigned_string):
+    # 开始计算签名
+    key = RSA.importKey(open(private_key_path).read())
+    signer = PKCS1_v1_5.new(key)
+    signature = signer.sign(SHA256.new(unsigned_string.encode("utf8")))
+    # base64 编码，转换为unicode表示并移除回车,python2.7中改为encodestring
+    sign = base64.encodestring(signature).decode("utf8").replace("\n", "")
+    return sign
+
+signed_string = sign_string('app_private_key.pem', "abc\n")    
+
 print ('{"a":"123"}')
 print sign_read_type1(json)
 print ('a=123, read pem file')
 print sign_read_type1('a=123')
 print ('a=123, read pem file, with packing')
 print sign_read_type2('a=123')
-
+print ('abc\n, read pem file, with packing')
+print sign_read_type2('abc\n')
+print signed_string  
 # def sign(data_file_name, signature_file_name, private_key_file_name):
 #   """
 #     签名函数使用指定的私钥Key对文件进行签名，并将签名结果写入文件中
